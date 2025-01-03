6e915d0d24cc0557ddaf7ea9b195d229025c170f
Bjorn <bp@datakortet.no>
2025-01-03 16:11:03 +0100

    Add env serializer and export to file.

--- seeqret/seeqret_add.py
+++ seeqret/seeqret_add.py
@@ -1,22 +1,21 @@
 import click
-import requests
 
 from seeqret.storage.sqlite_storage import SqliteStorage
 
 
-def fetch_pubkey_from_url(url):
-    # FIXME: this does not provide a way to connect the user to the pubkey!
-    r = requests.get(url)
-    if r.status_code != 200:
-        ctx = None
-        try:
-            ctx = click.get_current_context()
-        except AttributeError:
-            # during testing we don't neccessarily have a context...
-            raise RuntimeError(f'Could not fetch pubkey from url: {url}')
-        ctx.fail(click.style(f'Failed to fetch public key: {url}', fg='red'))
-    click.secho('Public key fetched.', fg='green')
-    return r.text
+# def fetch_pubkey_from_url(url):
+#     # FIXME: this does not provide a way to connect the user to the pubkey!
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         ctx = None
+#         try:
+#             ctx = click.get_current_context()
+#         except AttributeError:
+#             # during testing we don't neccessarily have a context...
+#             raise RuntimeError(f'Could not fetch pubkey from url: {url}')
+#         ctx.fail(click.style(f'Failed to fetch public key: {url}', fg='red'))
+#     click.secho('Public key fetched.', fg='green')
+#     return r.text
 
 
 # seeqret add user ...
