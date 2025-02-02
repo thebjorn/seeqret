e39f9e1af86c8cb357cc309cefd66c94248eb866
Bjorn Pettersen <bp@datakortet.no>
2025-02-02 17:52:51 +0000

    flake8

--- seeqret/cli_group_add.py
+++ seeqret/cli_group_add.py
@@ -15,16 +15,20 @@ from .run_utils import seeqret_dir
               help='The app to add the secret to')
 @click.option('--env', default='*', show_default=True,
               help='The env(ironment) to add the secret to (e.g. dev/prod)')
-def key(ctx, name: str, value: str, app: str|None = None, env: str|None = None):  # noqa: F811
+def key(ctx, name: str, value: str, app: str | None = None, env: str | None = None):  # noqa: F811
     """Add a new NAME -> VALUE mapping.
 
        You can (should) specify the app and environment properties when adding
        a new mapping.
     """
-    if ':' in name or (isinstance(app, str) and ':' in app) or (isinstance(env, str) and ':' in env):
+    if (':' in name
+            or (isinstance(app, str) and ':' in app)
+            or (isinstance(env, str) and ':' in env)):
+
         ctx.fail(click.style(
             'Colon `:` is not valid in key, app, or env', fg='red'
         ))
+
     click.secho(
         f'Adding a new key: {name}, value: {value}, app: {app}, env: {env}',
         fg='blue'
