dc51d51dec918576d4f3ed8b73ae79edfebe4cae
Bjørn Pettersen <bp@datakortet.no>
2026-07-18 16:03:35 +0200

    tagged-upversion

--- seeqret/__init__.py
+++ seeqret/__init__.py
@@ -1,6 +1,6 @@
 from seeqret.storage.get_secret import get_secret
 
-__version__ = '0.4.4'
+__version__ = '0.5.1'
 
 
 def get(key, app='*', env='*'):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Safely transferring code secrets.
 
 import setuptools
 
-version = '0.5.0'
+version = '0.5.1'
 
 setuptools.setup(
     name='seeqret',
